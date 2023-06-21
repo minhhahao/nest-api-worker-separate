f488881cc4e94862f88c516914ae5d8e4a4a8d82
ha.hao.minh <ha.hao.minh@sun-asterisk.com>
2023-06-21 22:22:08 +0700

    Refactor: Remove a file not used

--- src/processor.ts
+++
@@ -1,6 +0,0 @@
-import { Job, DoneCallback } from 'bull';
-
-export default function (job: Job, cb: DoneCallback) {
-  console.log(`[${process.pid}] ${JSON.stringify(job.data)}`);
-  cb(null, 'It works');
-}
